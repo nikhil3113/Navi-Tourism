cb7019e7f7be63e7d224be7c0d4c6e895796ddd6
nikhil3113 <nikhilgovindchavan@ternaengg.ac.in>
2024-06-06 22:44:12 +0530

    logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useDarkMode } from "../DarkModeContext";
 import { CiLight } from "react-icons/ci";
 import { MdDarkMode } from "react-icons/md";
+import logo from "../assets/map.svg";
 
 const NavBar = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
@@ -9,7 +10,19 @@ const NavBar = () => {
     <>
       <nav className=" py-3 shadow-[rgba(0,0,0 /0.1)_0px_10px_0px_0px] flex justify-between">
         <Link to="/">
-          <img src="" alt="logo" className="p-3" />
+          {/* <img src={logo} alt="logo" className="w-20"  /> */}
+          <div className="bg-transparent dark:bg-transparent h-12 ml-5 rounded-lg flex ">
+            <div className="flex justify-center items-center">
+              <img
+                src={logo}
+                alt=""
+                className="w-12 flex justify-center items-center"
+              />
+            </div>
+            <h1 className="text-2xl font-extrabold  text-black white mt-2 dark:text-white hidden xl:block md:block ">
+              Navi-Tourism
+            </h1>
+          </div>
         </Link>
 
         <div className="relative flex items-center cursor-pointer px-5">
